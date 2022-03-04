ba44f26cc5c4c239e1c44b09030c4beb702c986c
Guilherme Gouveia <guilhermegouveia.dev@gmail.com>
2022-03-04 17:52:01 -0300

    Fix 'end_line catacter error script' at scripts payara

--- config/script/payara/deploy_backend_script.sh
+++ config/script/payara/deploy_backend_script.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-echo -e "AS_ADMIN_PASSWORD=admin" > password
-asadmin -u admin --passwordfile password deploy /opt/payara41/deployments/backend.war
+echo -e "AS_ADMIN_PASSWORD=admin" > password; asadmin -u admin --passwordfile password deploy /opt/payara41/deployments/backend.war
