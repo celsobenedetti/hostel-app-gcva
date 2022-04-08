84807ffe3830f2ca417e2e18c3d79defdaca297c
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-04-07 22:11:49 -0300

    Revert for local database

--- config/docker/payara/bin/mysql_connection.sh
+++ config/docker/payara/bin/mysql_connection.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "AS_ADMIN_PASSWORD=admin" > password
 asadmin -u admin --passwordfile password add-library ../lib/mysql-connector-java-8.0.28.jar; \
-asadmin -u admin --passwordfile password create-jdbc-connection-pool --datasourceclassname com.mysql.cj.jdbc.MysqlDataSource --restype javax.sql.DataSource --property User=root:Password=password:DatabaseName=hostelDB:ServerName=34.207.73.254:Port=32769:UseSSL=false:AllowPublicKeyRetrieval=true mysql-pool; \
+asadmin -u admin --passwordfile password create-jdbc-connection-pool --datasourceclassname com.mysql.cj.jdbc.MysqlDataSource --restype javax.sql.DataSource --property User=root:Password=password:DatabaseName=hostelDB:ServerName=hostel-app-database:Port=3306:UseSSL=false:AllowPublicKeyRetrieval=true mysql-pool; \
 asadmin -u admin --passwordfile password create-jdbc-resource --connectionpoolid mysql-pool jdbc/mysql-pool
